203d66cc665f2771abb29a6fb1db10602dffe728
spoorthy2025 <spoorthyk23@gmail.com>
2026-04-29 19:19:27 -0800

    Update create, concatenate.py

--- create, concatenate.py
+++ create, concatenate.py
@@ -4,3 +4,11 @@ print("First string is : ",s1)
 print("Second string is : ",s2)
 print("concatenations of two strings :",s1+s2)
 print("Substring of given string :",s1[1:4])
+
+output:
+Enter first String : spoorthy
+Enter second String : k
+First string is :  spoorthy
+Second string is :  k
+concatenations of two strings : spoorthyk
+Substring of given string : poo
