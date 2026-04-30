ad76589d36db06edba7f0fdf3553404d9b274682
spoorthy2025 <spoorthyk23@gmail.com>
2026-04-29 19:17:45 -0800

    Update different Arithmetic Operations on numbers in Python.py

--- different Arithmetic Operations on numbers in Python.py
+++ different Arithmetic Operations on numbers in Python.py
@@ -7,3 +7,15 @@ print("Division of a and b ",a/b)
 print("Remainder of a and b ",a%b)
 print("Exponent of a and b ",a**b)
 print("Floar division of a and b ",a//b)
+
+output:
+
+Enter a value 8
+Enter b value 8
+Addition of a and b  16
+Subtraction of a and b  0
+Multiplication of a and b  64
+Division of a and b  1.0
+Remainder of a and b  0
+Exponent of a and b  16777216
+Floar division of a and b  1
